f3044aabf713ed54308242ec104d6e2b1a41e067
James Coliz <jcoliz@outlook.com>
2025-03-15 13:29:40 -0700

    feat: Add string formats

--- cli/Program.cs
+++ cli/Program.cs
@@ -13,30 +13,62 @@ var node = await JsonNode.ParseAsync(stream!);
 var streamDeclarations = node!["properties"]!["streamDeclarations"];
 
 Console.WriteLine("components:");
-Console.WriteLine("\tschema:");
+Console.WriteLine("  schema:");
 
 foreach (var declaration in streamDeclarations!.AsObject())
 {
-    Console.WriteLine("\t\t{0}:", declaration.Key);
-    Console.WriteLine("\t\t\ttype: object");
-    Console.WriteLine("\t\t\tadditionalProperties: false");
-    Console.WriteLine("\t\t\tproperties:");
+    Console.WriteLine("    {0}:", declaration.Key);
+    Console.WriteLine("      type: object");
+    Console.WriteLine("      additionalProperties: false");
+    Console.WriteLine("      properties:");
 
     var columns_json = declaration.Value!["columns"];
 
     var columns = JsonSerializer.Deserialize<List<Column>>(columns_json!.ToJsonString());
 
-    foreach(var column in columns)
+    foreach(var column in columns!)
     {
-        Console.WriteLine("\t\t\t\t{0}:", column.name);
-        Console.WriteLine("\t\t\t\t\ttype: {0}", column.type);
-        Console.WriteLine("\t\t\t\t\tdescription: {0}", column.description);
+        Console.WriteLine("        {0}:", column.name);
+        Console.WriteLine("          description: {0}", column.description);
+
+        switch (column.type)
+        {
+            case "real":
+                if (column.description!.Contains("integer") || column.description!.Contains("number"))
+                {
+                    Console.WriteLine("          type: integer");
+                }
+                else
+                {
+                    Console.WriteLine("          type: number");
+                    Console.WriteLine("          format: double");
+                }
+                break;
+            case "datetime":
+                Console.WriteLine("          type: string");
+                Console.WriteLine("          format: date-time");
+                break;
+            case "string":
+                Console.WriteLine("          type: string");
+                if (column.description!.Contains("unique"))
+                {
+                    Console.WriteLine("          format: guid");
+                }
+                else if (column.description!.Contains("timestamp"))
+                {
+                    Console.WriteLine("          format: date-time");
+                }
+                break;
+            default:
+                Console.WriteLine("          type: {0}", column.type);
+                break;
+        }
     }
 }
 
 public record Column
 {
-    public string name { get; set; }
-    public string type { get; set; }
-    public string description { get; set; }
+    public string name { get; set; } = string.Empty;
+    public string type { get; set; } = string.Empty;
+    public string description { get; set; } = string.Empty;
 };
